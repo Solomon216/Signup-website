81a7d3f778802426f807e8df0ea8ea50297db3ae
unknown <solomonparthi3@gmail.com>
2023-07-07 15:50:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const Bp = require('body-parser')
+const request=require('request')
+const app = express();
+const client = require("@mailchimp/mailchimp_marketing");
+
+//app.use(express.static("public"))
+app.use(Bp.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'\\signup.html')
+})
+
+app.post('/', (req, res) => {
+    var fn = req.body.fnam;
+    var ln = req.body.lnam;
+    var mail = req.body.mailid;
+    
+    client.setConfig({
+        apiKey: '1d815879434743bfa4f70dc50c42ce8e-us11',
+        server: 'us11',
+    });
+
+    const run = async () => {
+        const response = await client.lists.addListMember('2b4bf7afae', {
+            email_address: mail,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fn,
+                LNAME:ln
+            }
+
+        });
+        console.log(response);
+    };
+
+    run();
+    if (res.statusCode===200) {
+        res.sendFile(__dirname+"\\success.html")
+    } else {
+        res.sendFile(__dirname+"\\failure.html")
+    }
+})
+app.post('/failure', (req, res) => {
+    res.redirect('/')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server started");
+})
